9970404715eac6c67ab935ffca0f97c34d356151
Phạm Khánh Duy <phamkhanhduy0207@gmail.com>
2023-04-13 16:12:48 +0700

    Add index19.html

---
+++ JS/14.js
@@ -0,0 +1,45 @@
+function b1() {
+    let browser = prompt("Enter browser name!")
+    switch (browser) {
+        case 'Edge':
+            alert("You've got the Edge!");
+            break;
+        case 'Chrome':
+        case 'Firefox':
+        case 'Safari':
+        case 'Opera':
+            alert('Okay we support these browsers too');
+            break;
+        default:
+            alert('We hope that this page looks ok!');
+    }
+}
+function b2() {
+    let a = +prompt('a?', '');
+    switch (a) {
+        case 0:
+            alert(0);
+            break;
+        case 1:
+            alert(1);
+            break;
+        case 2:
+        case 3:
+            alert('2,3');
+            break;
+    }
+}
+function b3() {
+    let value = +prompt('Type a number', 0);
+    switch ((value>0)?1:(value<0)?-1:0) {
+        case 1:
+            alert(1);
+            break;
+        case -1:
+            alert(-1);
+            break;
+        case 0:
+            alert(0);
+            break;
+    }
+}
