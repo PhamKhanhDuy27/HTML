2978effbca209f587eeeab22495ef6d5dc13150c
Phạm Khánh Duy <phamkhanhduy0207@gmail.com>
2023-04-17 09:37:07 +0700

    Add index25.html

--- JS/19.js
+++ JS/19.js
@@ -1,10 +1,19 @@
 function dayOfMonth() {
     let month = parseInt(document.getElementById("month").value);
     switch (month) {
-        case 1, 3, 5, 7, 8, 10, 12:
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
             document.getElementById("result").innerHTML = "Tháng " + month + " có 31 ngày";
             break;
-        case 4, 6, 9, 11:
+        case 4:
+        case 6:
+        case 9:
+        case 11:
             document.getElementById("result").innerHTML = "Tháng " + month + " có 30 ngày";
             break;
         case 2:
